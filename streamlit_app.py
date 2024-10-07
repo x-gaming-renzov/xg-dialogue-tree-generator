bff24ca9dfaf9f535986ec6ee6e5e06ffdd89966
pixelatedBrad <paras@sif.design>
2024-10-07 22:18:30 +0530

    Update instructions and information in the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,10 @@ st.info('This is a simple app to generate dialogue trees using the XG format.')
 
 with st.expander('How to use this app:'):
     st.write('''
-    1. Enter the dialogue tree in the text box.
-    2. Click the button to generate the XG dialogue tree.
-    3. The generated XG dialogue tree will be displayed in the text box below.
+    1. Enter Personality of NPC. It can be anything like moody, hilarious, etc.
+    2. Give background of NPC. It could be why this npc is in the game, what they are doing, etc.
+    3. Depth : Length of conversation.
+    4. PlayerTag : Here you will enter conditions for player to enter certain branch of conversation.
+             eg. if player has item x, go to branch 1, else go to branch 2.
+    5. EndGoal : What you want to achieve from this conversation. Default : End conversation.
     ''')
